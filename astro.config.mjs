025fc62c5867942e2434440f72b11f84a9f051fa
Martin Hooper <martin@mjhooper.co.uk>
2025-04-18 20:32:37 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,30 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.google(),
+              name: "Comic Neue",
+              cssVariable: "--font-comic"
+          },
+          {
+              provider: fontProviders.google(),
+              name: "Audiowide",
+              cssVariable: "--font-audiowide"
+          },
+          {
+              provider: fontProviders.google(),
+              name: "Quantico",
+              cssVariable: "--font-quantico"
+      }]
+  },
 
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.google(),
-                name: "Satisfy",
-                cssVariable: "--font-satisfy"
-        }]
-    }
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
